41c6ab82269f4d59526a0c5d9722f6130b227e81
delbertina <6349928+delbertina@users.noreply.github.com>
2023-11-26 15:25:10 -0600

    Add basic scaffolding

---
+++ src/drawing/animal.py
@@ -0,0 +1,16 @@
+import turtle
+
+def fish_basic():
+    print("fish basic")
+    
+def lion_adult_male():
+    print("lion adult male")
+    
+def bird_basic():
+    print("bird basic")
+    
+def snake_basic():
+    print("snake basic")
+
+def cat_basic():
+    print("cat basic")
---
+++ src/main.py
@@ -0,0 +1,10 @@
+import turtle
+import math
+from drawing.animal import *
+
+# Get user input
+drawingPrompt = input("Enter prompt: ")
+
+print(drawingPrompt)
+
+cat_basic()
